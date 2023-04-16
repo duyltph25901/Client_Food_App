9f53058a0205c79c7b6364aba6edb4577407e891
Duy Thanh <lothanhduy2003@gmail.com>
2023-04-17 00:08:59 +0700

    Add screens

--- App.js
+++ App.js
@@ -2,13 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {
   Welcome,
-  Walk_1,
-  Walk_2,
-  Walk_3
+  Walk_1, Walk_2, Walk_3,
+  SignIn, SignUp, ForgotPassword,
 } from './Screens/index'
 
 export default function App() {
   return (
-    <Walk_3 />
+    <SignIn />
   );
 }

34e48eaed40d409c559a87393a112a21930d84ac
Duy Thanh <lothanhduy2003@gmail.com>
2023-04-21 14:47:04 +0700

    create new screen

--- Screens/Auth/SignIn.js
+++ Screens/Auth/SignIn.js
@@ -1,9 +1,94 @@
-import { StyleSheet, Text, View, SafeAreaView, Dimensions, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView, Dimensions, TextInput, TouchableOpacity, Alert } from 'react-native'
+import React, { useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const width = Dimensions.get('window').width
 
 const SignIn = () => {
+    const navigation = useNavigation()
+    const url = 'http://192.168.1.14:3000/api/v1'
+    const login = '/handle-login-user'
+    const [email, setEmail] = useState('beedyto@gmail.com   ')
+    const [pass, setPass] = useState('123123')
+
+    const handleLogin = async () => {
+        const user = {
+            email: email,
+            password: pass
+        }
+        const urlLogin = `${url}${login}`
+
+        await fetch(urlLogin, {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then((res) => {
+                const code = res.status
+
+                console.log(
+                    `
+                    \n>>>>> Check code login: ${code}\n
+                    `
+                )
+
+                if (code === 404) {
+                    Alert.alert('Opps', 'Không tìm thấy email!')
+                    return false
+                } if (code == 405) {
+                    Alert.alert('Opps', 'Mật khẩu không đúng!')
+                    return false
+                }
+
+                return res.json()
+            })
+            .then((data) => {
+                const objectCurrent = data.objectCurrent
+
+                console.log(
+                    `
+                    \n>>>>> Check data res login: ${JSON.stringify(objectCurrent)}\n
+                    `
+                )
+
+                if (!objectCurrent) return false
+
+                clearAsyncStorage()
+                storeData(objectCurrent)
+
+                navigation.navigate('Home')
+            })
+            .catch((err) => {
+                Alert.alert('Opps', err)
+            })
+    }
+
+    const clearAsyncStorage = async () => {
+        try {
+            await AsyncStorage.clear()
+            console.log('\n>>>>> Message: AsyncStorage cleared\n');
+        } catch (error) {
+            console.error(`
+                \n>>>>> Message: ${error}\n
+            `)
+        }
+    }
+
+    const storeData = async (value) => {
+        try {
+            const jsonValue = JSON.stringify(value)
+            await AsyncStorage.setItem('ObjectCurrent', jsonValue)
+
+            console.log(`\n>>>>> Message: Save object current by async storage successful!\n`)
+        } catch (e) {
+            console.log(`\n>>>>> Message: ${e}!\n`)
+        }
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.header}>
@@ -11,31 +96,41 @@ const SignIn = () => {
                     fontSize: 34,
                     fontWeight: 'bold',
                     color: 'white'
-                }}>Login</Text>
+                }}>Đăng nhập</Text>
             </View>
             <View style={styles.body}>
-                <View style={styles.bodyChild}>
+                <View style={{
+                    paddingVertical: 50,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
                     <Text style={{
                         fontSize: 24,
                         fontWeight: 'bold',
                         color: '#EEA743',
                         marginBottom: 10
                     }}>
-                        Welcome back
+                        Chào mừng bạn quay trở lại
                     </Text>
                     <Text style={{
                         fontSize: 18,
                         fontWeight: 'bold',
                         color: '#686868'
                     }}>
-                        Login to your account
+                        Đăng nhập tài khoản của bạn
                     </Text>
                 </View>
                 <View style={styles.bodyChild}>
                     <TextInput style={styles.input}
-                        placeholder='Email' />
+                        placeholder='Email'
+                        value={email}
+                        onChangeText={(text) => setEmail(text)}
+                        keyboardType='email-address' />
                     <TextInput style={styles.input}
-                        placeholder='Password' />
+                        placeholder='Mật khẩu'
+                        secureTextEntry={true}
+                        value={pass}
+                        onChangeText={(text) => setPass(text)} />
                     <View style={{
                         width: width - 30,
                         justifyContent: 'flex-end',
@@ -43,8 +138,11 @@ const SignIn = () => {
                     }}>
                         <TouchableOpacity style={{
                             padding: 12,
-                        }}>
-                            <Text>Forgot password?</Text>
+                        }}
+                            onPress={() => {
+                                navigation.navigate('Forgotpassword')
+                            }}>
+                            <Text>Quên mật khẩu?</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
@@ -54,8 +152,9 @@ const SignIn = () => {
                         justifyContent: 'center',
                         alignItems: 'center'
                     }}>
-                        <TouchableOpacity style={styles.containerButton}>
-                            <Text style={styles.textButton}>Login</Text>
+                        <TouchableOpacity style={styles.containerButton}
+                            onPress={handleLogin}>
+                            <Text style={styles.textButton}>Đăng nhập</Text>
                         </TouchableOpacity>
                     </View>
                     <View style={{
@@ -66,13 +165,15 @@ const SignIn = () => {
                     }}>
                         <Text style={{
                             fontSize: 12,
-                        }}>Don't have an account? </Text>
-                        <TouchableOpacity>
+                        }}>Bạn không có tài khoản?</Text>
+                        <TouchableOpacity onPress={() => {
+                            navigation.navigate('SignUp')
+                        }}>
                             <Text style={{
                                 fontSize: 14,
                                 fontWeight: 'bold',
                                 color: '#EEA743'
-                            }}>SIGN UP</Text>
+                            }}>    ĐĂNG KÍ</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
--- Screens/Auth/SignUp.js
+++ Screens/Auth/SignUp.js
@@ -1,9 +1,79 @@
-import { StyleSheet, Text, View, SafeAreaView, Dimensions, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView, Dimensions, TextInput, TouchableOpacity, Alert } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import validate from '../../controllers/Validate'
 
 const width = Dimensions.get('window').width
 
 const SignUp = () => {
+    const navigation = useNavigation()
+    const [userName, setUserName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [phoneNumber, setPhoneNumber] = useState('')
+    const url = 'http://192.168.137.1:3000/api/v1'
+    const signUp = '/handle-sign-up-user'
+
+    const handleSignUp = async () => {
+        const urlSignUp = `${url}${signUp}`
+
+        const user = {
+            userName: userName.trim(),
+            email: email.toLocaleLowerCase().trim(),
+            password: password.trim(),
+            phoneNumber: phoneNumber.trim()
+        }
+
+        // validate
+        if (!userName || !email || !password || !phoneNumber) {
+            Alert.alert('Opps', 'Vui lòng điền đầy đủ thông tin!')
+            return false
+        } if (!validate.validateEmail(email)) {
+            Alert.alert('Opps', 'Email không đúng định dạng!')
+            return false
+        } if (!validate.validatePhoneNumber(phoneNumber)) {
+            Alert.alert('Opps', 'Số điện thoại không hợp lệ!')
+            return false
+        } if (!validate.validatePassword(password)) {
+            Alert.alert('Opps', 'Mật khẩu phải chứa tối thiểu 6 kí tự bao gồm cả chữ cái viết hoa, viết thường và kí tự đặc biệt!')
+            return false
+        }
+
+        // handle Đăng kí
+        await fetch(urlSignUp, {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then((res) => {
+                const code = res.status
+                if (code == 400) {
+                    Alert.alert('Opps', 'Email này đã được đăng kí!')
+                    return false
+                } if (code == 401) {
+                    Alert.alert('Opps', 'Số điện thoại này đã được đăng kí!')
+                    return false
+                }
+
+                Alert.alert('Thành công', 'Đăng kí tài khoản thành công!')
+                clearInput()
+            })
+            .catch((err) => {
+                Alert.alert('Oppps', err.message)
+            })
+    }
+
+    const clearInput = () => {
+        setEmail('')
+        setPassword('')
+        setUserName('')
+        setPhoneNumber('')
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.header}>
@@ -12,43 +82,68 @@ const SignUp = () => {
                     fontWeight: 'bold',
                     color: 'white',
                     marginBottom: 10
-                }}>Sign up</Text>
+                }}>Đăng kí</Text>
                 <Text style={{
                     color: 'white',
                     fontSize: 14
-                }}>Create your new account</Text>
+                }}>Tạo mới tài khoản của bạn</Text>
             </View>
             <View style={styles.body}>
-                <View style={styles.bodyChild}>
-                    <TextInput style={styles.input}
-                        placeholder='User name' />
-                    <TextInput style={styles.input}
-                        placeholder='Email' />
-                    <TextInput style={styles.input}
-                        placeholder='Password' />
-                    <TextInput style={styles.input}
-                        placeholder='Phone number' />
-                    <TouchableOpacity style={styles.containerButton}>
-                        <Text style={styles.textButton}>Create new account</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={{
-                    flex: 1.5,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    flexDirection: 'row'
+                <KeyboardAwareScrollView style={{
+                    flex: 1,
+                    width: width,
                 }}>
-                    <Text style={{
-                        fontSize: 12,
-                    }}>You aldready have an account?</Text>
-                    <TouchableOpacity>
-                        <Text style={{
-                            fontSize: 14,
-                            fontWeight: 'bold',
-                            color: '#EEA743'
-                        }}>SIGN IN</Text>
-                    </TouchableOpacity>
-                </View>
+                    <View style={{
+                        flex: 1,
+                        height: 510,
+                    }}>
+                        <View style={styles.bodyChild}>
+                            <TextInput style={styles.input}
+                                placeholder='Tên đăng nhập'
+                                value={userName}
+                                onChangeText={(text) => setUserName(text)} />
+                            <TextInput style={styles.input}
+                                keyboardType='email-address'
+                                placeholder='Email'
+                                value={email}
+                                onChangeText={(text) => setEmail(text)} />
+                            <TextInput style={styles.input}
+                                secureTextEntry={true}
+                                keyboardType='visible-password'
+                                placeholder='Mật khẩu'
+                                value={password}
+                                onChangeText={(text) => setPassword(text)} />
+                            <TextInput style={styles.input}
+                                keyboardType='phone-pad'
+                                placeholder='Số điện thoại'
+                                value={phoneNumber}
+                                onChangeText={(text) => setPhoneNumber(text)} />
+                            <TouchableOpacity style={styles.containerButton}
+                                onPress={handleSignUp}>
+                                <Text style={styles.textButton}>Đăng kí</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={{
+                            flex: 1.5,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            flexDirection: 'row'
+                        }}>
+                            <Text style={{
+                                fontSize: 12,
+                            }}>Bạn đã có tài khoản?</Text>
+                            <TouchableOpacity onPress={() => {
+                                navigation.goBack()
+                            }}>
+                                <Text style={{
+                                    fontSize: 14,
+                                    fontWeight: 'bold',
+                                    color: '#EEA743'
+                                }}>    ĐĂNG NHẬP</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </KeyboardAwareScrollView>
             </View>
         </SafeAreaView>
     )

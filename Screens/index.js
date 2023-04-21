0c06d101de152131477fadfc260e436c49be1f87
Duy Thanh <lothanhduy2003@gmail.com>
2023-04-21 14:48:28 +0700

    import new screen

--- Screens/index.js
+++ Screens/index.js
@@ -1,9 +1,11 @@
 import { Welcome } from './Welcome/index'
 import { Walk_1, Walk_2, Walk_3 } from './WalkThrough/index'
 import { SignIn, SignUp, ForgotPassword } from './Auth/index'
+import { ContainerHomeScreen } from './Home'
 
 export {
     Welcome,
     Walk_1, Walk_2, Walk_3,
     SignIn, SignUp, ForgotPassword,
+    ContainerHomeScreen
 }

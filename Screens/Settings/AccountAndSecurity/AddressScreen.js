1ba365d3c7ef445a52bb3bef24e34c73702d9445
Duy Thanh <lothanhduy2003@gmail.com>
2023-05-01 22:43:14 +0700

    create new screen

---
+++ Screens/Settings/AccountAndSecurity/AddressScreen.js
@@ -0,0 +1,14 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AddressScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>AddressScreen</Text>
+        </SafeAreaView>
+    )
+}
+
+export default AddressScreen
+
+const styles = StyleSheet.create({})
---
+++ Screens/Settings/Supported/SuppoterCenter.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SuppoterCenter = () => {
+    return (
+        <View>
+            <Text>SuppoterCenter</Text>
+        </View>
+    )
+}
+
+export default SuppoterCenter
+
+const styles = StyleSheet.create({})
